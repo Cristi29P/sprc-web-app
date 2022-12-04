c715461ea47bc937361e232b2acb8cce2c9244b9
Cristian Paris <cristian.tanasep@gmail.com>
2022-12-04 11:47:43 +0200

    Add initial files

---
+++ src/main/java/com/sprc_web_app/SprcWebAppApplication.java
@@ -0,0 +1,13 @@
+package com.sprc_web_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprcWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SprcWebAppApplication.class, args);
+	}
+
+}
